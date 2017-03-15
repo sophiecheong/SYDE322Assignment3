130261f440465347f245b2c9edc5842b3cd1a637
chsophie <cheongsophie@gmail.com>
2017-03-15 17:44:47 -0400

    Added feature for searching available drones

--- src/server/apiRoutes.js
+++ src/server/apiRoutes.js
@@ -3,12 +3,6 @@ const router = express.Router();
 const path = require('path');
 const mysqlConnection = require('./sqlConnection');
 
-// middleware that is specific to this router
-// router.use(function timeLog (req, res, next) {
-//   console.log('Time: ', Date.now());
-//   next();
-// });
-
 router.post('/client', function (req, res) {
 	const query = 'INSERT INTO Client (clientName, clientAddress, finesOutstanding) VALUES (?, ?, ?)';
 	const values = [ req.body.clientName, req.body.clientAddress, req.body.clientFines ];	
@@ -22,29 +16,52 @@ router.post('/client', function (req, res) {
 });
 
 router.get('/drones', function (req, res) {
-	const query = 'SELECT * FROM Drone ' + getSearchConditions(req.query) + ';';
+	var options = "";
+	if(!!req.query.startDate) 
+		options += "(b.dateDue<'"+req.query.startDate+"' OR b.dateDue IS NULL) AND (m.endDate<'"+req.query.startDate+"' OR m.endDate IS NULL)";
+	if(!!req.query.endDate) {
+		if(!!options) options += " AND ";
+		options += "(b.dateTaken>'" + req.query.endDate + "' OR b.dateTaken IS NULL) AND (m.startDate>'" + req.query.endDate + "' OR m.startDate IS NULL)";
+	}
+	if(!!req.query.droneType) {
+		if(!!options) options += " AND ";
+		options += "Drone.droneType='" + req.query.droneType + "'";
+	}
+	if(!!req.query.manufacturer){
+		if(!!options) options += " AND ";
+		options += "Drone.manufacturer='" + req.query.manufacturer + "'";
+	}
+	if(!!req.query.range){
+		if(!!options) options += " AND ";
+		options += "Drone.range='" + req.query.range + "'";
+	}
+	const query = 'SELECT Drone.* FROM Drone ' +
+					'LEFT JOIN Booking AS b ON Drone.droneID = b.droneID ' +
+					'LEFT JOIN MaintenanceRecord as m ON Drone.droneID = m.droneID' + 
+					(!!options ? " WHERE " + options : "");
+
 	mysqlConnection.runQuery(query, function (err, results){
 		if (!!err) {
 			console.log("query error: ", err);    //Show error in terminal
 			res.status(500).json({ error: err }).end();
 		}
-		res.status(200).json({ data: results }).end();
+		res.status(200).json({ data: results.map(item => { 
+				item.available = !!item.available; 
+				return item })
+		}).end();
 	});
 });
 
 function getSearchConditions(options) {
-	if(!Object.keys(options).length) 
-		return '';
+	if(!Object.keys(options).length) return '';
 
-	var clause = "WHERE";
+	var clause = "";
+
 	for(var option in options) {
-		if("WHERE" == clause) 
-			clause += " " + option + "='" + options[option] + "'";
-		else
-			clause += " AND " + option + "='" + options[option] + "'";
+		if(!clause) clause += " " + option + "='" + options[option] + "'";
+		else clause += " AND " + option + "='" + options[option] + "'";
 	}
-	return clause;
-
+	return !!clause? "WHERE" + clause : '' ;
 }
 
 module.exports = router;
